279e38f76ba81299bd1c28963d5da243df8ff56a
pauloppp <apaulob.sk@gmail.com>
2022-02-03 16:31:31 -0300

    Acertos Diversos Create/Update/Delete/Index

--- ESTACIONAMENTO/wwwroot/js/site.js
+++ ESTACIONAMENTO/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function HabilitaStatus() {
+    document.getElementById("status").disabled = false;
+    document.getElementById("classificacao").disabled = false;
+}
+
+function HabilitaTodos() {
+    document.getElementById("DataSaida").disabled = false;
+    document.getElementById("DataEntrada").disabled = false;
+    document.getElementById("Classificacao").disabled = false;
+    document.getElementById("Status").disabled = false;
+    document.getElementById("CarroModelo").disabled = false;
+    document.getElementById("ManobristaNome").disabled = false;
+    document.getElementById("fechar").submit();
+}
